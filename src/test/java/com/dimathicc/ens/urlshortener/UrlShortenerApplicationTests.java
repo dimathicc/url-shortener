34eb4b9f2339a44db7e2e4a3e296d554bcefda4b
Дмитрий Швалев <shvalevdd@mail.ru>
2024-03-01 10:46:06 +0300

    init url-shortener

---
+++ src/test/java/com/dimathicc/ens/urlshortener/UrlShortenerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dimathicc.ens.urlshortener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlShortenerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
